3e4f21ca2ae733955b6733b65159e717af4688db
PopTt <alanhoablan@gmail.com>
2022-08-18 20:58:39 +0800

    back function

--- App.js
+++ App.js
@@ -1,7 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, BackHandler, Alert } from 'react-native';
 
 export default function App() {
+  useEffect(() => {
+    const backAction = () => {
+      Alert.alert("Exit flexBox?", [
+        {
+          text: "No",
+          onPress: () => null,
+          style: "cancel"
+        },
+        { text: "YES", 
+          onPress: () => BackHandler.exitApp() }
+      ]);
+      return true;
+    };
+
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      backAction
+    );
+
+    return () => backHandler.remove();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>

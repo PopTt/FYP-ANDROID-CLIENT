945015e3d8591d6735fd723600100dd9a3b1a99e
PopTt <alanhoablan@gmail.com>
2022-08-20 17:56:34 +0800

    Update FaceRegister.js

--- src/screens/auth/FaceRegister.js
+++ src/screens/auth/FaceRegister.js
@@ -1,9 +1,18 @@
 import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { Camera } from 'expo-camera'
 import * as FaceDetector from 'expo-face-detector'
 
 const FaceRegister = () => {
+  const [cameraPermission, setCameraPermission] = useState(null)
+  const [faceData, setFaceData] = useState(null)
+
+  useEffect(() => {
+    async () => {
+      
+    }
+  })
+
   return (
     <View>
       <Text>FaceRegister</Text>

a8865fbe1979894c8733929577f7c172f56f07a7
PopTt <alanhoablan@gmail.com>
2023-03-27 03:01:39 +0800

    Update Instance.js

--- src/axios/Instance.js
+++ src/axios/Instance.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 //baseURL = 'http://192.168.2.101:5000/'
-baseURL = 'https://fyp-app-server.herokuapp.com/'
+baseURL = 'https://fyp2-server.herokuapp.com/'
 testURL = 'http://192.168.0.212:5000/'
 
-const instance = axios.create({ baseURL: testURL });
+const instance = axios.create({ baseURL: baseURL });
 
 export {instance}
